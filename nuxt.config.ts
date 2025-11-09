9b01b68a2705dcbb361046d57b1edbb39246ac12
Kevin Pallado <kevin.pallado97@gmail.com>
2025-11-10 02:52:34 +0800

    update nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,10 @@
 import tailwindcss from "@tailwindcss/vite";
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  ssr: true, // or false for static
+  nitro: {
+    preset: "aws-amplify", // Optional but recommended
+  },
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
   modules: ["@nuxt/eslint", "@nuxt/image", "@nuxt/icon", "shadcn-nuxt"],
