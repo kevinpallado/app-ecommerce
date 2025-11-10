90d87403315446a1cc3356cff11ca8dc7387dfb3
Kevin Pallado <kevin.pallado97@gmail.com>
2025-11-10 16:12:01 +0800

    remove amplify

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,7 @@
 import tailwindcss from "@tailwindcss/vite";
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  ssr: true, // or false for static
-  nitro: {
-    preset: "aws-amplify", // Optional but recommended
-  },
+  ssr: true,
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
   modules: ["@nuxt/eslint", "@nuxt/image", "@nuxt/icon", "shadcn-nuxt"],
